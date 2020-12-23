5f6de7b1473fcf4b8b53540005da9bc134d32cb2
Nika <yavtushenkonika@gmail.com>
2020-12-23 18:34:08 +0200

    adds webpack

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export default {
+	timer: document.getElementById('#timer-1'),
+	days: document.querySelector('span[data-value="days"]'),
+	hours: document.querySelector('span[data-value="hours"]'),
+	mins: document.querySelector('span[data-value="mins"]'),
+	secs: document.querySelector('span[data-value="secs"]'),
+};
