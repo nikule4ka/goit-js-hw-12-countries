fc7c0d775e863a88f6868d2f53d4df4399ec881d
Nika <yavtushenkonika@gmail.com>
2020-12-26 00:36:56 +0200

    adds notifications

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import countriesId from './templates/countries.hbs';
 import debounce from 'lodash.debounce'
 import API from './js/fetchCountries';
 import getRefs from "./js/refs";
+import errorsNotifications from './js/notifications'
 
 const refs = getRefs();
 
 refs.searchForm.addEventListener('input', debounce(onSearch, 500));
 
-function onSearch(e) {
-	e.preventDefault();
-
+function onSearch() {
 	
 	const searchQuery = refs.searchForm.value;
+
+	if (!searchQuery) {
+		return
+	}
 
 	API.fetchCountries(searchQuery)
 		.then(checkFindItems)
@@ -24,25 +27,30 @@ function checkFindItems(country) {
 	refs.cardContainer.innerHTML = '';
 
 	if (country.length > 10) {
-		 alert('Something wrong');
-	} else if (country.length > 1 && country.lenth <= 10) {
-		renderCountriesList(country);
-	} else {
-		renderCountryCard(country);
+		return errorsNotifications(
+			'Nothing was found for your request.',
+			'Please enter another request!',
+		)
 	}
-};
-function renderCountriesList(country) {
-	const markup = countriesId(country);
-	refs.cardContainer.insertAdjacentHTML('beforeend', markup);
+	if (country.length > 1 || country.lenth <= 10) {
+		 renderCountriesList(country);
+	}
+	if (country.length === 1) {
+		// console.log(renderCountryCard(country[0]))
+		 renderCountryCard(country);
+	}
 };
 
 function renderCountryCard(country) {
 	const markupIdCard = oneCountryId(country);
-	
 	refs.cardContainer.insertAdjacentHTML('beforeend', markupIdCard);
+	
 };
 
-
+function renderCountriesList(country) {
+	const markup = countriesId(country);
+	refs.cardContainer.insertAdjacentHTML('beforeend', markup);
+};
 
 
 function onFetchError(error) {
