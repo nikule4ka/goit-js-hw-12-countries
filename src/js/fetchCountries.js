5ab27581f1b6ddd3c667fea27f8a8d510ec2ff60
Nika <yavtushenkonika@gmail.com>
2020-12-27 15:21:16 +0200

    adds error fetching data

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,13 @@
 function fetchCountries(searchQuery) {
 	return fetch(
 		`https://restcountries.eu/rest/v2/name/${searchQuery}`
-	).then(response => response.json());
+	).then(response => {
+		if (response.ok) {
+			return response.json()
+		} else {
+			return new Error('Error fetching data');
+		}
+	});
 }
 
 export default { fetchCountries };
